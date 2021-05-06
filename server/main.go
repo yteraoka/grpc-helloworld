2de3112f85dd97be79d16128b9d57297b171749a
Yoshinori Teraoka <teraoka@3-shake.com>
2021-05-06 13:37:32 +0900

    起動時に Listening port を表示

--- server/main.go
+++ server/main.go
@@ -61,6 +61,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 
 func main() {
 	flag.Parse()
+	log.Printf("Listening :%d\n", *port)
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
